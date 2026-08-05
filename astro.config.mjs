75c98b34c60b5a8638295aef221096f3e17dd5f1
David Lopez <jdll0403@gmail.com>
2026-08-04 23:44:42 -0600

    added footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/inicio": "/",
+    "/home": "/",
+    "/services": "/servicios",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
